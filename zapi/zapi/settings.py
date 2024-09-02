0e51767a8a44ddcb3082046118e0012d22bb4bdf
sheikhxzaheer <sheikhzaheer037@gmail.com>
2024-09-02 13:05:24 +0530

    n

--- zapi/zapi/settings.py
+++ zapi/zapi/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-$2s79$7o3eo6iqye8y-%bj_ityjk9_qqw9l9)=ygsp9he53la-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1", ".vercel.app", ".now.sh"]
 
 
 # Application definition
@@ -74,12 +74,24 @@ WSGI_APPLICATION = 'zapi.wsgi.application'
 
 
 # Database
-# https://docs.djangoproject.com/en/5.1/ref/settings/#databases
+# # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'verceldb',
+        'USER': 'default',
+        'PASSWORD': 'zU0cw2lGpNBA',
+        'HOST': 'ep-steep-glade-805441-pooler.ap-southeast-1.postgres.vercel-storage.com',
+        'PORT': '',
+        
     }
 }
 
