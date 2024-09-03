3b86a9967a25dff120cfe27ae2f44d4cf545f511
sheikhxzaheer <sheikhzaheer037@gmail.com>
2024-09-03 12:57:38 +0530

    n

--- zapi/zapi/settings.py
+++ zapi/zapi/settings.py
@@ -133,7 +133,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 MEDIA_URL = '/media/'
 
 # Default primary key field type
