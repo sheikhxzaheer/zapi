8fec1bacef65bd05e8b86eddbff0cfce9cc451fc
sheikhxzaheer <sheikhzaheer037@gmail.com>
2024-09-03 12:36:29 +0530

    n

--- zapi/zapi/urls.py
+++ zapi/zapi/urls.py
@@ -18,6 +18,11 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf.urls.static import static
 from django.conf import settings
+from django.conf.urls.static import static
+
+admin.site.site_header = "Sheikh Zaheer"
+admin.site.site_title = "Sheikh Zaheer"
+admin.site.index_title = "Sheikh Zaheer"
 
 urlpatterns = [
     path('admin/', admin.site.urls),
